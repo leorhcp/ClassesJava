ea7cf47c3a28997c17672ea7c634c7642d2ed46e
Leonel <leorhcp1@gmail.com>
2023-04-29 22:09:35 -0300

    Getters and Setters

--- CursoJava.java
+++ CursoJava.java
@@ -4,5 +4,11 @@ public class CursoJava{
         Jogador j1 = new Jogador("Leonel", 10);
         Jogador j2 = new Jogador("Silvio", 13);
         Jogador j3 = new Jogador("Ronaldo", 9);
+
+        j1.addVidas();
+        j1.addVidas();
+        j1.addVidas();
+        j1.addVidas();
+        System.out.println("Vidas do jogador "+j1.getNome()+":"+ j1.getVidas());
     }
 }
--- Jogador.java
+++ Jogador.java
@@ -1,12 +1,51 @@
-import java.rmi.StubNotFoundException;
-
-import javax.sound.sampled.SourceDataLine;
-
 public class Jogador {
 
+    private final int maxVidas=3; 
+    private String nome;
+    private int num = 0;
+    private int vidas = 0;
+      
     //Criação de método construtor:
     public Jogador(String nome, int num){
+        
+        this.nome=nome;
+        this.num=num;
+        this.vidas=0;     
         System.out.printf("Jogador %s, camisa %d criado.%n",nome, num);
+
+    }
+    //Getters and Setters:
+    public String getNome(){
+        return this.nome;
+    }
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+
+    public int getNum(){
+        return this.num;
+    }
+    public void setNum(int num){
+        this.num = num;
+    }
+
+    public int getVidas(){
+        return this.vidas;
+    }
+    public void setVidas(int vidas){
+        if(vidas > maxVidas){
+            this.vidas = maxVidas;
+        } else if(vidas < 0){
+            this.vidas = 0;
+        } else{
+            this.vidas=vidas;
+        }      
+    }
+
+    public void addVidas(){
+        if(this.vidas < maxVidas){
+            this.vidas ++;
+        }
 
     }
     
